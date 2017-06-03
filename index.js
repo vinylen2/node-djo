8edff6821e83df798687aef38cb077cc2b210097
vinylen2 <gabrielwallen93@gmail.com>
2017-06-03 22:25:42 +0200

    better filenames

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let app = new Koa();
 // Set up body parsing middleware
 app.use(bodyParser());
 
-// Require the Router defined in router.js
-const words = require('./js/router.js');
+// Require the Router defined in words.js
+const words = require('./routes/words.js');
 
 // Use the Router on the sub route /movies
 app.use(words.routes());
